00f61042a4d0db8419caf6b16e732648fdc5de9e
HotariTobu <hotari24tools@gmail.com>
2023-11-04 14:38:48 +0900

    first commit

---
+++ WPFHotariStyles/Converters/DoubleDivisionConverter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WPF_Test
+{
+    public class DoubleDivisionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => (double)value / (double)parameter == 0 ? 1 : (double)parameter;
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => (double)value * (double)parameter == 0 ? 1 : (double)parameter;
+    }
+}
